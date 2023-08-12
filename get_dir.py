7d81f9bdbb5e7e6a1667959bf38aae4bab58702b
focusq8 <31973454+focusq8@users.noreply.github.com>
2023-08-13 00:28:06 +0300

    Update get_dir.py

--- get_dir.py
+++ get_dir.py
@@ -3,80 +3,46 @@ from os import system
 
 Red = "\033[1;31;40m"
 Blue = "\033[1;36;40m"
-green = '\033[1;32;40m'
 Yellow="\033[0;33m"
-White="\033[0;37m"
 
-def get_dir():
-        
-    system(f"title Basic Tool To Get Directories Site")
-    system('cls||clear')
-    host = input(f"""{green}
-For example: http://testphp.vulnweb.com
-
-
-
-Enter your url please: """)
-
-    file = input("\nEnter your file please: ")
-    with open(file, 'r') as lists:
+try:
+    with open("wordlist.txt", 'r') as lists:
         wordslist = lists.read().splitlines()
+except FileNotFoundError:
+    input("Couldn't find wordlist.txt!")
 
-    system('cls||clear')
-    try:
-        for words in wordslist:
-            url =f"{host}/{words}"
-            req = requests.get(url)
+url =  input("Enter Your Site: ")
+system('cls||clear')
 
-            if req.status_code == 200:
-                print(f"{Blue} Found: {url}\n")
-            
-            elif req.status_code == 404:
-                print(f"{Red} Not Found: {url}\n")
+if 'https://' in url:
+  url
+else:
+  url = 'http://'+url
+
+for lists in wordslist:
+        add = url+"/"+lists
+        try:
+            geturl = requests.get(add)
+            if "Page Not Found" in geturl.text:
+                print(f"{Red}page not found {add}")
+                with open("found.txt", 'a') as file:
+                    file.write(f"Page Not Found {add}\n")
+                    continue
+
+            elif geturl.status_code == 200:
+                print(f"{Blue}Found {add}")
+                with open("found.txt", 'a') as file:
+                    file.write(f"Found {add}\n")
+                    continue
+
+            elif geturl.status_code == 403:
+                print(f"{Yellow}Forbidden {add}")
+                with open("Forbidden.txt", 'a') as file:
+                    file.write(f"Forbidden {add}\n")
+                    continue            
             else:
-                print(f"{Yellow} Error: {req.status_code} {url}\n")
-
-        input(f"{White}\n\n\nFinished......")
-    except:
-        print("exit")
-        exit()
-    
-def sub_dir():
-
-    system(f"title Basic Tool To Get Directories Site")
-    system('cls||clear')
-    host = input(f"""{green}
-For example: vulnweb.com
-
-
-
-Enter your url please: """)
-
-    file = input("\nEnter your file please: ")
-    with open(file, 'r') as lists:
-        wordslist = lists.read().splitlines()
-
-    system('cls||clear')
-    try:
-        for words in wordslist:
-            url ="http://"+words+"."+host
-            
-            try:
-                req = requests.get(url)
-
-                if req.status_code == 200:
-                    print(f"{Blue} Found: {url}\n")
+                print(f"{Red}Not Found {add}")
                 
-                elif req.status_code == 400:
-                    print(f"{Red} Not Found: {url}\n")
-                else:
-                    print(f"{Yellow} Error: {req.status_code} {url}\n")
-            except requests.ConnectionError:
-                pass
-
-        input(f"{White}\n\n\nFinished......")
-    except:
-        print("exit")
-        exit()
-
-sub_dir()
+        except:
+            print(f"{Red} Error: {geturl.status_code} {add}\n")
+input("Finished !!!")
